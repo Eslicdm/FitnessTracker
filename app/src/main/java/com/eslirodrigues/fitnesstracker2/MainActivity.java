ce482011fc1c4d4d720d6f39dadd24d88e4b91b5
Eslicdm <eslicdm1@gmail.com>
2020-11-25 15:16:30 -0300

    Adding a Menu selectable

--- app/src/main/java/com/eslirodrigues/fitnesstracker2/MainActivity.java
+++ app/src/main/java/com/eslirodrigues/fitnesstracker2/MainActivity.java
@@ -5,33 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView rvMain = findViewById(R.id.recycler_main);
+        rvMain = findViewById(R.id.recycler_main);
+
+        List<MainItems> mainItems = new ArrayList<>();
+        mainItems.add(new MainItems(1, R.string.label_imc, R.drawable.ic_baseline_wb_sunny_24, Color.GREEN));
+        mainItems.add(new MainItems(2, R.string.label_tmb, R.drawable.ic_baseline_visibility_24, Color.YELLOW));
 
         rvMain.setLayoutManager(new GridLayoutManager(this, 2));
-        MainAdapter adapter = new MainAdapter();
+        MainAdapter adapter = new MainAdapter(mainItems);
+        adapter.setListener(id -> {
+            switch (id) {
+                case 1:
+                    startActivity(new Intent(MainActivity.this, ImcActivity.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(MainActivity.this, TmbActivity.class));
+                    break;
+            }
+        });
         rvMain.setAdapter(adapter);
     }
 
-    public class MainHolder extends RecyclerView.ViewHolder {
+    public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainHolder> {
 
-        public MainHolder(@NonNull View itemView) {
-            super(itemView);
+        List<MainItems> mainItems;
+        OnItemClickListener listener;
+
+        public MainAdapter(List<MainItems> mainItems) {
+            this.mainItems = mainItems;
         }
-    }
 
-    public class MainAdapter extends RecyclerView.Adapter<MainHolder> {
-
+        public void setListener(OnItemClickListener listener) {
+            this.listener = listener;
+        }
 
         @NonNull
         @Override
@@ -41,12 +68,34 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull MainHolder holder, int position) {
-
+            MainItems mainItem = mainItems.get(position);
+            holder.bind(mainItem);
         }
 
         @Override
         public int getItemCount() {
-            return 5;
+            return mainItems.size();
+        }
+
+        public class MainHolder extends RecyclerView.ViewHolder {
+
+            public MainHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+
+            public void bind(MainItems mainItem) {
+                TextView txtLabel = itemView.findViewById(R.id.item_txt);
+                ImageView imgLabel = itemView.findViewById(R.id.item_img);
+                LinearLayout btnChoice = itemView.findViewById(R.id.item_layout);
+
+                btnChoice.setOnClickListener(view -> {
+                    listener.onClick(mainItem.getIdLabel());
+                });
+
+                txtLabel.setText(mainItem.getTextLabel());
+                imgLabel.setImageResource(mainItem.getImageLabel());
+                btnChoice.setBackgroundColor(mainItem.getColorLabel());
+            }
         }
     }
 }
