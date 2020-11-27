f7d45a8da9c6362fee800e4be7ff7998921bb684
Eslicdm <eslicdm1@gmail.com>
2020-11-27 06:38:37 -0300

    creating a validate function

--- app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
+++ app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
@@ -3,13 +3,26 @@ package com.eslirodrigues.fitnesstracker2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ImcActivity extends AppCompatActivity {
+
+    private EditText editHeight;
+    private EditText editWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc);
 
+    }
+
+    private boolean validate() {
+        return (!editHeight.getText().toString().isEmpty()
+            && !editHeight.getText().toString().startsWith("0")
+            && !editWeight.getText().toString().isEmpty()
+            && !editWeight.getText().toString().startsWith("0"));
     }
 }
