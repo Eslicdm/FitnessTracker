ac3689c9451934b109d91c127c74abaeac602bc2
Eslicdm <eslicdm1@gmail.com>
2020-11-28 06:14:11 -0300

    Adding a response imc and positive button

--- app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
+++ app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
@@ -1,5 +1,6 @@
 package com.eslirodrigues.fitnesstracker2;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,13 +34,37 @@ public class ImcActivity extends AppCompatActivity {
             int height = Integer.parseInt(editHeight.getText().toString());
 
             double imcResult = calculateImc(weight, height);
+            int imcResponse = responseImc(imcResult);
 
             AlertDialog dialog = new AlertDialog.Builder(this)
                     .setTitle(getString(R.string.imc_response, imcResult))
+                    .setMessage(imcResponse)
+                    .setPositiveButton(android.R.string.ok, (dialog1, which) -> {})
                     .create();
 
             dialog.show();
         });
+    }
+
+    @StringRes
+    private int responseImc(double imc) {
+        if (imc < 15) {
+            return R.string.imc_severely_low_weight;
+        } else if (imc < 16) {
+            return R.string.imc_very_low_weight;
+        } else if (imc < 18.5) {
+            return R.string.imc_low_weight;
+        } else if (imc < 25) {
+            return R.string.normal;
+        } else if (imc < 30) {
+            return R.string.imc_high_weight;
+        } else if (imc < 35) {
+            return R.string.imc_so_high_weight;
+        } else if (imc < 40) {
+            return R.string.imc_severely_high_weight;
+        } else {
+            return R.string.imc_extreme_weight;
+        }
     }
 
     private double calculateImc(int weight, int height) {
