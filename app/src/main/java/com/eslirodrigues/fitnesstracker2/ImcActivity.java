28f57da93000e8ad7ce07bf4880fcb8bd00c80d2
Eslicdm <eslicdm1@gmail.com>
2020-11-30 19:47:36 -0300

    creating a list menu for results

--- app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
+++ app/src/main/java/com/eslirodrigues/fitnesstracker2/ImcActivity.java
@@ -1,5 +1,6 @@
 package com.eslirodrigues.fitnesstracker2;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,6 +71,23 @@ public class ImcActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListCalcActivity.class);
         intent.putExtra("type", "imc");
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu:
+                openListCalcActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @StringRes
