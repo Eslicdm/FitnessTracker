02c934501ff33ee708a2333f2a1446083bcfab13
Eslicdm <eslicdm1@gmail.com>
2020-12-02 09:06:23 -0300

    creating a tmb calculator

--- app/src/main/java/com/eslirodrigues/fitnesstracker2/TmbActivity.java
+++ app/src/main/java/com/eslirodrigues/fitnesstracker2/TmbActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class TmbActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class TmbActivity extends AppCompatActivity {
     private EditText editTmbWeight;
     private EditText editTmbAge;
     private Button btnSendTmb;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +28,44 @@ public class TmbActivity extends AppCompatActivity {
         editTmbHeight = findViewById(R.id.edit_height_tmb);
         editTmbAge = findViewById(R.id.edit_age_tmb);
         btnSendTmb = findViewById(R.id.btn_send_tmb);
+        spinner = findViewById(R.id.spinner_tmb);
 
         btnSendTmb.setOnClickListener(view -> {
             if (!validate()) {
                 Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show();
                 return;
             }
+
+            int height = Integer.parseInt(editTmbHeight.getText().toString());
+            int weight = Integer.parseInt(editTmbWeight.getText().toString());
+            int age = Integer.parseInt(editTmbAge.getText().toString());
+
+            double tmbResult = calculateTmb(height, weight, age);
+
+            double tmbTotal = tmbResponse(tmbResult);
         });
+    }
+
+    private double calculateTmb(int height, int weight, int age) {
+        return 66 + (weight * 13.8) + (5 * height) - (6.8 * age);
+    }
+
+    private double tmbResponse(double tmb) {
+        int index = spinner.getSelectedItemPosition();
+        switch (index) {
+            case 0:
+                return tmb * 1.2;
+            case 1:
+                return tmb * 1.375;
+            case 2:
+                return tmb * 1.55;
+            case 3:
+                return tmb * 1.725;
+            case 4:
+                return tmb * 1.9;
+            default:
+                return 0;
+        }
     }
 
     private boolean validate() {
